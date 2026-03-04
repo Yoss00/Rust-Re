88710b4d0c22fe3eb1e462cc321d2f8c30e66d7a
Yosua Trujillo <josstrujca899@gmail.com>
2026-03-03 21:38:20 -0600

    Aprendiendo ownerShip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Hola");
+
+    let s2  = &s1;
+
+    println!("El valor de s2 es: {}", s2);
+    println!("Y el valor de s1 es: {}", s1);
 }
